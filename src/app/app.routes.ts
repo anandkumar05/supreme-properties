8f60916b118639bf915c50eb96772a29c6116741
anandkumar05 <anand.ak2526@gmail.com>
2024-08-24 15:40:23 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { ProjectComponent } from './project/project.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'service',component: ServiceComponent },
+    { path: 'project', component: ProjectComponent },
+    { path: 'contact', component: ContactComponent }
+];
